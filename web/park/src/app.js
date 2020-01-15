277b9b55d1895cf7300bf480a2444a71f9605ba0
cxl619 <547087795@qq.com>
2020-01-15 17:44:11 +0800

    router 修改

--- web/park/src/app.js
+++ web/park/src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Routes from './router/router.js'
-import {Provider} from 'react-redux'
 
 ReactDom.render((
   <Routes></Routes>
--- web/park/src/router/router.js
+++ web/park/src/router/router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import Index from './../pages/index.js'
 import Data from './../pages/data.js'
 
