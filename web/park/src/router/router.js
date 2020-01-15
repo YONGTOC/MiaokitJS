a16862141eb6389b5dff02db74f31563d9c19362
cxl619 <547087795@qq.com>
2020-01-15 17:48:14 +0800

    Revert "router 修改"
    This reverts commit 277b9b55d1895cf7300bf480a2444a71f9605ba0.

--- web/park/src/router/router.js
+++ web/park/src/router/router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Index from './../pages/index.js'
 import Data from './../pages/data.js'
 
