02d0a411ef9c89168e957a918da648f3b30e444c
cxl <547087795@qq.com>
2020-03-03 16:18:29 +0800

    i Signed-off-by: cxl <547087795@qq.com>

--- web/private/mPark/view/Index.tsx
+++ web/private/mPark/view/Index.tsx
@@ -2,14 +2,14 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import Home from "home";
- 
+
 
 import ParkCompany from "parkCompany";
-import Photograph from "photograph"; 
+import Photograph from "photograph";
 
-import InfoArea from "infoArea"; 
-import Message from "message"; 
-import AboutMe from "aboutMe"; 
+import InfoArea from "infoArea";
+import Message from "message";
+import AboutMe from "aboutMe";
 
 
 import "css!./styles/view.css"
@@ -17,24 +17,25 @@ import { HashRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 
 
-class Index extends React.Component{
-    constructor(props) {
-        super(props);
- 
-    }
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
 
-    public componentDidMount(){
-}
+  }
 
-    public three(){}
+  public componentDidMount() {
+  }
 
-    public render() {
-        return (
-            <div className={"web"} >
-                <Home /> 
-            </div>
-            )
-    }
+  public three() { }
+
+  public render() {
+    return (
+      <div className={"web"} >
+        <Home />
+
+      </div>
+    )
+  }
 
 }
 
@@ -48,18 +49,18 @@ export default Index;
 //Index，3d沙盘； 微圈 --infoArea； 资讯--message；我的 --aboutMe；
 
 ReactDOM.render(
-    <HashRouter>
-        <Switch>
-            <Route exact path="/parkCompany" component={ParkCompany} />
-            <Route exact path="/photograph" component={Photograph} />
+  <HashRouter>
+    <Switch>
+      <Route exact path="/parkCompany" component={ParkCompany} />
+      <Route exact path="/photograph" component={Photograph} />
 
-            <Route exact path="/" component={Index} />
-            <Route exact path="/infoArea" component={InfoArea} />
-            <Route exact path="/message" component={Message} />
-            <Route exact path="/aboutMe" component={AboutMe} />
-        </Switch>
-    </HashRouter>
-    , document.getElementById('viewContainer'));
+      <Route exact path="/" component={Index} />
+
+      <Route exact path="/infoArea" component={InfoArea} />
+      <Route exact path="/message" component={Message} />
+      <Route exact path="/aboutMe" component={AboutMe} />
+    </Switch>
+  </HashRouter>
+  , document.getElementById('viewContainer'));
 
 
- 
